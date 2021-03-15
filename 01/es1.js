8395b36e543aaec574ce0615b7f613470754ba72
Enrico <bravienrico@yahoo.it>
2021-03-15 13:13:25 +0100

    added test and dayjs module

--- 01/es1.js
+++ 01/es1.js
@@ -1,11 +1,16 @@
 'use strict'
+const dayjs = require('dayjs');
 
-function Task(id, description, urgent = false, private = true, deadline){
+function Task(id, description, urgent = false, private_ = true, deadline){
     this.id = id;
     this.description = description;
     this.urgent = urgent;
-    this.private = private;
+    this.private_ = private_;
     this.deadline = deadline;
+
+    this.toString = () => {
+        return `Task -> Id = ${this.id}, Description = ${this.description}, Urgent = ${this.urgent}, Private = ${this.private_}, Dealine = ${this.deadline || "undefined"}\n`;
+    };
 }
 
 function Tasks() {
@@ -26,6 +31,7 @@ function Tasks() {
         });
 
         this.list = [...definedDeadline, ...notDefinedDeadline]; 
+        console.log(`${this.list}`);
     };
 
     this.filterAndPrint = () => {
@@ -34,3 +40,18 @@ function Tasks() {
     };
 
 }
+
+const task1 = new Task(1, 'Fare la spesa', false, true, dayjs("2021-03-25"));
+const task2 = new Task(2, 'Dentista', true, true, dayjs("2021-03-12"));
+const task3 = new Task(3, 'Meccanico', false, true);
+const task4 = new Task(4, 'Lezione', false, false, dayjs("2021-04-01"));
+const task5 = new Task(5, 'Appuntamento in banca');
+
+let tasksList = new Tasks();
+tasksList.add(task1);
+tasksList.add(task2);
+tasksList.add(task3);
+tasksList.add(task4);
+tasksList.add(task5);
+
+tasksList.sortAndPrint();
