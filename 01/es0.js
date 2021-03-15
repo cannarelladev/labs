fd22f7ef3a130dfed752240b58d500f9880ecb2a
cannarelladev <cannarella.dev@gmail.com>
2021-03-15 11:41:16 +0100

    fix merge

--- 01/es0.js
+++ 01/es0.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict'
 
 const array = ['spring', 'winter', 'car', 'a', 'in', 'politecnico'];
@@ -17,24 +16,3 @@ for(let i=0; i<array.length; i++){
 console.log(array);
 
 //debugger;
-=======
-"use strict";
-
-const name1 = ["spring", "winter", "summer", "a", "cia", "al"];
-
-console.log(name1);
-
-const reduction = (array) => {
-    for (let i=0; i < array.length; i++) {
-        if (array[i].length < 2) {
-            array[i] = "";
-        } else {
-            array[i] = `${array[i][0]}${array[i][1]}${array[i][array[i].length-2]}${array[i][array[i].length-1]}`;
-        }
-    }
-}
-
-reduction(name1);
-
-console.log(name1);
->>>>>>> 7e7c38e6c11cc88f1f29cf5c014c89ad99052f86
